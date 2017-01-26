d268fd2a6d9144d78f48b1de2c8f2a8cfa2de4ff
Christian Leo-Pernold <mazedlx@gmail.com>
2017-01-26 10:53:26 +0100

    Added Prop for styling the input element.
    The input class can now be passed as a prop. E.g. `vue-timepicker input-class="form-control"></vue-timepicker>`

--- src/vue-timepicker.vue
+++ src/vue-timepicker.vue
@@ -10,6 +10,7 @@ export default {
   name: 'VueTimepicker',
 
   props: {
+    inputClass: {type: String},
     value: {type: Object},
     hideClearButton: {type: Boolean},
     format: {type: String},
@@ -373,7 +374,7 @@ export default {
 
 <template>
 <span class="time-picker">
-  <input class="display-time" :id="id" v-model="displayTime" @click.stop="toggleDropdown" type="text" readonly />
+  <input :class="inputClass" :id="id" v-model="displayTime" @click.stop="toggleDropdown" type="text" readonly />
   <span class="clear-btn" v-if="!hideClearButton" v-show="!showDropdown && showClearBtn" @click.stop="clearTime">&times;</span>
   <div class="time-picker-overlay" v-if="showDropdown" @click.stop="toggleDropdown"></div>
   <div class="dropdown" v-show="showDropdown">
